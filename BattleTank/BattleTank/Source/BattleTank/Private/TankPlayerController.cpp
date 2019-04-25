33a68aa40ea7b21b765c8aba5d0cb60cf8e36e16
HysanDev <hysanyu120122@gmail.com>
2019-04-25 15:13:25 +0800

    BattleTank (0.0.8-dev) [Added log on beginplay]

--- BattleTank/BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,23 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+	}
+	UE_LOG(LogTemp, Error, TEXT("PlayerController Begin Play"));
+}
+
+
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
