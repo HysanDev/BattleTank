2eebe3ea523f333ea1b5dab41d0cb83895671d20
Hysan <hysandev@gmail.com>
2019-05-26 10:40:29 +0800

    BattleTank (0.0.29-dev) [Added Throttle]

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,10 @@
+// BattleTank by Hysan. For educational purposes only.
+
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+}
---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,20 @@
+// BattleTank by Hysan. For educational purposes only.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+};
